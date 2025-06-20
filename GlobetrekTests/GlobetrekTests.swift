1477939403e0a1d6218ff270a8e7a4f011fe34bc
Sheymakita123 <makitashey@gmail.com>
2025-06-20 05:41:52 +0100

    Add files via upload

---
+++ GlobetrekTests/GlobetrekTests.swift
@@ -0,0 +1,17 @@
+//
+//  GlobetrekTests.swift
+//  GlobetrekTests
+//
+//  Created by Shey Makita on 22/03/2025.
+//
+
+import Testing
+@testable import Globetrek
+
+struct GlobetrekTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
